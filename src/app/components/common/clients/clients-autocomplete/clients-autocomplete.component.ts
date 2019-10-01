06ec99dcb481261b5604f8bcb1cea5fa9b9dfe97
dev <dev@ravimoshark.com>
2019-10-01 12:22:33 +0200

    Finished client autocomplete. Created a base autocomple class.

--- src/app/components/common/clients/clients-autocomplete/clients-autocomplete.component.ts
+++ src/app/components/common/clients/clients-autocomplete/clients-autocomplete.component.ts
@@ -1,79 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-// tslint:disable: no-implicit-dependencies
-import { BaseInputFormComponent } from '@app/models/base-input.class';
-import { environment } from '@env/environment';
-import { Clients, ClientsService, InlineResponse200 } from '@ravimosharksas/apis-contract-libs-typescript';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { debounceTime, finalize, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+// tslint:disable-next-line:no-implicit-dependencies
+import { BaseAutocompleteComponent } from '@app/models/base-autocomplete';
+import { Clients, ClientsService } from '@ravimosharksas/apis-contract-libs-typescript';
+import { NGXLogger } from 'ngx-logger';
+import { map } from 'rxjs/operators';
 
-declare var require: any;
 @Component({
   selector: 'app-clients-autocomplete',
   templateUrl: './clients-autocomplete.component.html',
   styleUrls: ['./clients-autocomplete.component.scss']
 })
-export class ClientsAutocompleteComponent extends BaseInputFormComponent implements OnInit {
+export class ClientsAutocompleteComponent extends BaseAutocompleteComponent<Clients>
+  implements OnInit {
 
-  @Input() useGlobal = true;
-
-  options: Array<Clients> = [];
-
-  public filteredOptions = new BehaviorSubject<Array<Clients>>([]);
-  public filteredOptions$: Observable<Array<Clients>>;
-
-  public loading = new BehaviorSubject<boolean>(false);
-  public loading$ = this.loading.asObservable();
-
-  constructor(public readonly clientService: ClientsService) {
-    super();
-    if (!environment.production) {
-      // tslint:disable-next-line:no-require-imports
-      this.options.push(require('../../../../../../test/mock_data/clients.json'));
-    }
+  constructor(private readonly clientService: ClientsService,
+              logger: NGXLogger) {
+    super(logger, 'CLIENTS_AUTOCOMPLETE', 'clients');
   }
 
   ngOnInit(): void {
     super.ngOnInit();
-    if (this.useGlobal) {
-    // this.filteredOptions =
-    this.parent.form.controls[this.controlName].valueChanges
-      .pipe(
-        startWith(''),
-        // map(value => this._filter(value))
-        map(value => typeof value === 'string' ? value : value),
-        map(name => name ? this._filter(name) : this.options.slice())
-      );
-    } else {
-      this.parent.form.controls[this.controlName].valueChanges
-      .pipe(
-        debounceTime(500),
-        tap(() => {
-          this.filteredOptions.next([]);
-          this.loading.next(true);
-        })
-        , switchMap(value => this.clientService.getClients(0, 10, undefined, 'refClient like' + value)
-          .pipe(
-            finalize(() => {
-              this.loading.next(false);
-            })
-          )
-        )
-      )
-      .subscribe((data: InlineResponse200) => {
-        this.filteredOptions.next(data.items);
-      });
-    }
   }
 
-  private _filter(name: Clients | string): Array<Clients> {
-    if (typeof name === 'object') {
-      return [];
-    }
-    const filterValue = name.toLowerCase();
+  protected _filter(value: string): Array<Clients> {
+    const filterValue = value.toLowerCase();
 
     return this.options.filter((option: Clients) => {
       if (option.refClient && option.refClient.toLowerCase()
-        .includes(filterValue)) {
+          .includes(filterValue)
+      ) {
         return true;
       }
       if (option.name && option.name.toLowerCase()
@@ -87,20 +42,17 @@ export class ClientsAutocompleteComponent extends BaseInputFormComponent impleme
     });
   }
 
-  displayFn(client?: Clients): string | undefined {
+  protected _apiCall(entity: any): import('rxjs').Observable<Array<Clients>> {
+    return this.clientService
+      .getClients(0, 10, undefined, entity)
+      .pipe(map(value => value.items));
+  }
+
+  public identify(entity: Clients): string {
+    return entity.refClient;
+  }
+
+  public displayFn(client?: Clients): string | undefined {
     return client ? `${client.refClient} - ${client.name}` : undefined;
-  }
-
-  clear(): void {
-    if (this.parent.form.get(this.controlName).enabled) {
-      this.parent.form.get(this.controlName)
-        .setValue('');
-      this.parent.form.get(this.controlName)
-        .markAsUntouched();
-    }
-  }
-
-  identify(item: Clients): String {
-    return item.refClient;
   }
 }
---
+++ src/app/models/base-autocomplete.ts
@@ -0,0 +1,102 @@
+import { Input, OnInit } from '@angular/core';
+// tslint:disable-next-line:no-implicit-dependencies
+import { environment } from '@env/environment';
+import { NGXLogger } from 'ngx-logger';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, finalize, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { BaseInputFormComponent } from './base-input.class';
+
+declare var require: any;
+export abstract class BaseAutocompleteComponent<T> extends BaseInputFormComponent
+  implements OnInit {
+
+  @Input() useGlobal = true;
+
+  public filteredOptions = new BehaviorSubject<Array<T>>([]);
+  public filteredOptions$ = this.filteredOptions.asObservable();
+
+  public loading = new BehaviorSubject<boolean>(false);
+  public loading$ = this.loading.asObservable();
+
+  options: Array<T> = [];
+
+  constructor(
+    private readonly logger: NGXLogger,
+    protected readonly COMPONENT_NAME: string,
+    jsonFile?: string
+  ) {
+    super();
+    if (!environment.production) {
+      logger.debug(COMPONENT_NAME, 'adding data from mock json...');
+      if (jsonFile) {
+        // tslint:disable-next-line:no-require-imports
+        this.options.push(
+          ...require('../../../test/mock_data/'.concat(
+            jsonFile,
+            '.json'
+          ))
+        );
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    if (this.useGlobal) {
+      this.parent.form.controls[this.controlName].valueChanges
+      .pipe(
+        // debounceTime(500),
+        startWith(''),
+        distinctUntilChanged(),
+        map(value => typeof value === 'string' ? value : this.displayFn(value)),
+        map(client => client ? this._filter(client) : this.options.slice()))
+        .subscribe((clients: Array<T>) => {
+          this.filteredOptions.next(clients);
+      })
+      ;
+    } else {
+      this.parent.form.controls[this.controlName].valueChanges
+      .pipe(
+        debounceTime(500)
+        , tap(() => {
+          this.filteredOptions.next([]);
+          this.loading.next(true);
+        })
+        , switchMap(value => this._apiCall(value)
+          .pipe(
+            finalize(() => {
+              this.loading.next(false);
+            })
+          )
+        )
+      )
+      .subscribe(data => {
+        this.filteredOptions.next(data);
+      });
+    }
+  }
+
+  clear(): void {
+    if (this.parent.form.get(this.controlName).enabled) {
+      this.parent.form.get(this.controlName)
+        .setValue('');
+      this.parent.form.get(this.controlName)
+        .markAsUntouched();
+    }
+  }
+
+  onKeydownEvent(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {// escape pressed
+      this.clear();
+    }
+ }
+
+  public log(message: any, ...additional: Array<any>): void {
+    this.logger.debug(this.COMPONENT_NAME, message, additional);
+  }
+
+  protected abstract _filter(value: string): Array<T>;
+  protected abstract _apiCall(filterBy: string): Observable<Array<T>>;
+  public abstract identify(entity: T): string;
+  public abstract displayFn(entity: T): string | undefined;
+}
