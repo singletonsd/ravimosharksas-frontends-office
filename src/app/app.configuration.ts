88fe2e3b0132e62fcaa188998fc202f06e15d858
dev <dev@ravimoshark.com>
2019-10-10 13:23:09 +0200

    Setup API configuration.

--- src/app/app.configuration.ts
+++ src/app/app.configuration.ts
@@ -1,8 +1,10 @@
+import { environment } from '@env/environment';
 import { Configuration, ConfigurationParameters } from '@ravimosharksas/apis-contract-libs-typescript';
 
 // tslint:disable-next-line: only-arrow-functions
 export function apiConfigFactory(): Configuration {
   const params: ConfigurationParameters = {
+    basePath: environment.apiRavimoContract.API_BASE_PATH
   };
 
   return new Configuration(params);
