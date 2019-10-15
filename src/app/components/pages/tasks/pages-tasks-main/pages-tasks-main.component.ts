0a0295ba049ce61014c916c20653403670a15573
dev <dev@ravimoshark.com>
2019-10-15 17:23:44 +0200

    Fixed lint. Fixed missing translation.

--- src/app/components/pages/tasks/pages-tasks-main/pages-tasks-main.component.ts
+++ src/app/components/pages/tasks/pages-tasks-main/pages-tasks-main.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesTasksMainComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
