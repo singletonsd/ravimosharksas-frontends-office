95664d1ef5b5a14e41ab7cd62a4fe053f68df690
Patricio Rafael Perpetua <patricio.perpetua.arg@gmail.com>
2019-10-21 22:09:05 +0200

    Improved task add form for first submit.

--- src/app/components/common/tasks/tasks-add-form/tasks-add-form.component.ts
+++ src/app/components/common/tasks/tasks-add-form/tasks-add-form.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, Input } from '@angular/core';
 // tslint:disable: no-implicit-dependencies
 import { BaseFormAddComponent } from '@app/models/base-form-add.class';
 import { environment } from '@env/environment';
+import { TranslateService } from '@ngx-translate/core';
+import { Tasks, TasksService } from '@ravimosharksas/apis-task-libs-typescript';
 import { NGXLogger } from 'ngx-logger';
-// import { TranslateService } from '@ngx-translate/core';
 
 declare var require: any;
 @Component({
@@ -13,9 +14,11 @@ declare var require: any;
 })
 export class TasksAddFormComponent  extends BaseFormAddComponent<any> implements OnInit {
 
+  @Input() isSubmitting = false;
+
   constructor(logger: NGXLogger
-            // , private readonly translate: TranslateService
-            // , private readonly service: ContractsService
+            , private readonly translate: TranslateService
+            , private readonly service: TasksService
             , cdr: ChangeDetectorRef
             ) {
     super('CONTRACTS_ADD_FORM', 'models.contract.', cdr, logger, 'contract');
@@ -33,59 +36,51 @@ export class TasksAddFormComponent  extends BaseFormAddComponent<any> implements
   }
 
   finishSubmit(): void {
-    // const data: Contracts = {};
-    // Object.assign(data, this.form.getRawValue());
-    // for (const imported of data.importedMachines) {
-    //   if (typeof imported.contract === 'string' || typeof imported.contract === 'number') {
-    //     imported.contract = { refContract: imported.contract };
-    //   }
-    // }
-    // for (const location of data.locations) {
-    //   location.contract = !location.contract ? { refContract: data.refContract } : location.contract;
-    // }
-    // this.logger.debug(this.COMPONENT_NAME, 'form submitted.', data);
-    // if (this.item) {
-    //   this.logger.debug(this.COMPONENT_NAME, 'edit');
-    //   this.item = data;
-    //   this.service.editContract(data)
-    //   .subscribe(editedContract => {
-    //     this.translate.get('pages.contract.results.success.edit')
-    //       .subscribe(text => {
-    //         this.added.next(editedContract);
-    //         this.notification.success(text);
-    //         this.logger.debug(this.COMPONENT_NAME, 'success', editedContract);
-    //         this.finishAPICall(false);
-    //       });
-    //   }, error => {
-    //     const translate = (error && error.error && error.error.message ? error.error.message : 'fields.errors.unknown');
-    //     this.translate.get(translate)
-    //       .subscribe(text => {
-    //         this.notification.error(text);
-    //         this.logger.warn(this.COMPONENT_NAME, 'An error occur', error);
-    //         this.finishAPICall(false);
-    //       });
-    //   });
-    // } else {
-    //   this.logger.debug(this.COMPONENT_NAME, 'add');
-    //   this.service.addContract(data)
-    //   .subscribe(newItem => {
-    //     this.translate.get('pages.contract.results.success.add')
-    //       .subscribe(text => {
-    //         this.notification.success(text);
-    //         this.logger.debug(this.COMPONENT_NAME, 'success', newItem);
-    //         this.added.next(newItem);
-    //         this.finishAPICall(false);
-    //       });
-    //   }, error => {
-    //     const translate = (error && error.error && error.error.message ? error.error.message : 'fields.errors.unknown');
-    //     this.translate.get(translate)
-    //       .subscribe(text => {
-    //         this.notification.error(text);
-    //         this.logger.warn(this.COMPONENT_NAME, 'An error occur', error);
-    //         this.finishAPICall(false);
-    //       });
-    //   });
-    // }
+    const data: Tasks = {};
+    Object.assign(data, this.form.getRawValue());
+    this.logger.debug(this.COMPONENT_NAME, 'form submitted.', data);
+    if (this.item) {
+      this.logger.debug(this.COMPONENT_NAME, 'edit');
+      this.item = data;
+      this.service.editTask(data)
+      .subscribe(editedContract => {
+        this.translate.get('pages.task.results.success.edit')
+          .subscribe(text => {
+            this.added.next(editedContract);
+            this.notification.success(text);
+            this.logger.debug(this.COMPONENT_NAME, 'success', editedContract);
+            this.finishAPICall(false);
+          });
+      }, error => {
+        const translate = (error && error.error && error.error.message ? error.error.message : 'fields.errors.unknown');
+        this.translate.get(translate)
+          .subscribe(text => {
+            this.notification.error(text);
+            this.logger.warn(this.COMPONENT_NAME, 'An error occur', error);
+            this.finishAPICall(false);
+          });
+      });
+    } else {
+      this.logger.debug(this.COMPONENT_NAME, 'add');
+      this.service.addTask(data)
+      .subscribe(newItem => {
+        this.translate.get('pages.task.results.success.add')
+          .subscribe(text => {
+            this.notification.success(text);
+            this.logger.debug(this.COMPONENT_NAME, 'success', newItem);
+            this.added.next(newItem);
+            this.finishAPICall(false);
+          });
+      }, error => {
+        const translate = (error && error.error && error.error.message ? error.error.message : 'fields.errors.unknown');
+        this.translate.get(translate)
+          .subscribe(text => {
+            this.notification.error(text);
+            this.logger.warn(this.COMPONENT_NAME, 'An error occur', error);
+            this.finishAPICall(false);
+          });
+      });
+    }
 
   }
 
